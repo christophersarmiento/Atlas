411f1a2fd88bf9aea0a9060d085a53f5a73c9283
Christopher Sarmiento <csarmiento379@outlook.com>
2021-05-25 11:47:48 -0400

    Added ADMINISTRATOR permission for changing prefix.

--- src/commands/utility/prefix.js
+++ src/commands/utility/prefix.js
@@ -12,8 +12,7 @@ class PrefixCommand extends Command {
       },
       args: [
         {
-          id: 'prefix',
-          default: '.'
+          id: 'prefix'
         }
       ],
       channel: 'guild'
@@ -21,9 +20,18 @@ class PrefixCommand extends Command {
   }
 
   async exec(message,args) {
-    const old_prefix = this.client.settings.get(message.guild.id, 'prefix', '.');
-    await this.client.settings.set(message.guild.id, 'prefix', args.prefix);
-    return message.channel.send(`Prefix changed from \`${old_prefix}\` to \`${args.prefix}\``)
+    const old_prefix = this.client.settings.get(message.guild.id, "prefix", ".");
+    if (!args.prefix || args.prefix == old_prefix) {
+      return message.channel.send(`The current prefix is \`${old_prefix}\``);
+    }
+    
+    else if (message.guild.member(message.author).hasPermission("ADMINISTRATOR")) {
+      await this.client.settings.set(message.guild.id, "prefix", args.prefix);
+      return message.channel.send(`Prefix changed from \`${old_prefix}\` to \`${args.prefix}\``);
+    }
+
+    return message.channel.send(`Sorry, you do not have permisson to do that.`);
+    
   }
 }
 
