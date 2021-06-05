8bd5499a2d7eb995e4ef65d2c9b0eac7fd9f6a19
Christopher Sarmiento <csarmiento379@outlook.com>
2021-06-05 15:29:27 -0400

    Document quote and pagination files, remove dead code

--- src/commands/fun/quote.js
+++ src/commands/fun/quote.js
@@ -59,24 +59,26 @@ class QuoteCommand extends Command {
     });
   }
 
-  async slice_quotes(quotes, page_index) {
-    var page = quotes.slice(10 * (page_index - 1), 10 * page_index);
-    page = page.map((q) => `\`${quotes.indexOf(q) + 1}.\` ${q}`);
-    page = page.join("\n");
-    return page;
-  }
-
   async exec(message, args) {
+    // Pull quotes from datbase
     let allQuotes = this.client.settings.get(message.guild.id, "quotes");
     if (allQuotes == undefined) {
       allQuotes = [];
     }
 
-    // add quote
+    // Add quote
     if (args.add && !args.remove && !args.list && !args.index && !args.search && !args.purge) {
+      // If caller does not have Mangage Message Permissions
       if (!message.member.hasPermission("MANAGE_MESSAGES")) {
         return message.channel.send(`Sorry, you do not have the \`Manage Messages\` permisson.`);
       }
+
+      // If user did not suppy a quote to add
+      if (!args.quote) {
+        return message.channel.send("Please add a search query.");
+      }
+
+      // Add quote to array and update database
       let quote = args.quote.trim();
       allQuotes.push(quote);
       this.client.settings
@@ -95,22 +97,27 @@ class QuoteCommand extends Command {
         });
     }
 
-    // list quotes
+    // List quotes
     if (args.list && !args.add && !args.remove && !args.index && !args.search && !args.purge) {
+      // If there are no quotes in the server
       if (allQuotes.length <= 0) {
         return message.channel.send("There are no quotes for this server yet!");
       }
+      // Paginate quotes
       paginator(allQuotes, 10, 1, this.client.constants.infoEmbed, message, "Quotes");
     }
 
-    // quote remove
+    // Remove quote
     if (args.remove && !args.add && !args.list && args.index && !args.search && !args.purge) {
+      // If caller does not have manage messages permission
       if (!message.member.hasPermission("MANAGE_MESSAGES")) {
         return message.channel.send(`Sorry, you do not have the \`Manage Messages\` permisson.`);
       }
-      if (args.index != undefined) {
+      if (args.index) {
         let index = args.index - 1;
+        // If index is in range
         if (index >= 0 && index < allQuotes.length) {
+          // Remove the specified index and update database
           allQuotes.splice(index, 1);
           this.client.settings
             .set(message.guild.id, "quotes", allQuotes)
@@ -122,50 +129,66 @@ class QuoteCommand extends Command {
               return message.channel.send(this.client.failure);
             });
         } else {
+          // If specified index is out of range
           return message.channel.send(`Please enter a number between 1 and ${allQuotes.length}`);
         }
-      } else {
-        console.log("args index not found");
       }
-      return;
     }
 
-    // quote search
+    // Search quotes
     if (args.search && !args.add && !args.remove && !args.list && !args.index && !args.purge) {
+      // If no search query is specified
       if (!args.quote) {
         return message.channel.send("Please add a search query.");
       }
+
+      // Filter quotes that match the regex query
       let query = new RegExp(`${args.quote}`, "i");
       let matches = allQuotes.filter((q) => query.test(q));
+
+      // If there are quotes matching the search query
       if (matches.length > 0) {
+        // Paginate the results
         paginator(matches, 10, 1, this.client.constants.infoEmbed, message, `${matches.length} Matching Quotes`, allQuotes);
       } else {
+        // If there are no matching quotes
         let msg = new MessageEmbed().setColor(this.client.constants.infoEmbed).setDescription(`Could not find any quotes matching \`${args.quote}\``).setTitle("404!");
         return message.channel.send(msg);
       }
     }
 
-    // quote purge
+    // Purge quotes
     if (args.purge && !args.add && !args.remove && !args.list && !args.index && !args.search) {
+      // If caller does not have manage messages permission
       if (!message.member.hasPermission("MANAGE_MESSAGES")) {
         return message.channel.send(`Sorry, you do not have the \`Manage Messages\` permisson.`);
       }
+      // If no search query is specified
       if (!args.quote) {
         return message.channel.send("Please add a search query.");
       }
+
+      // Filter quotes that match the regex query
       let query = new RegExp(`${args.quote}`, "i");
       let matches = allQuotes.filter((q) => query.test(q));
+
+      // If there are quotes matching the search query
       if (matches.length > 0) {
+        // Paginate the results, and confirm the user wants to remove them from the database
         paginator(matches, 10, 1, this.client.constants.infoEmbed, message, `${matches.length} Matching Quotes`, allQuotes, true, this.client);
       } else {
+        // If there are no matching quotes
         let msg = new MessageEmbed().setColor(this.client.constants.infoEmbed).setDescription(`Could not find any quotes matching \`${args.quote}\``).setTitle("404!");
         return message.channel.send(msg);
       }
     }
 
-    // quote by index
+    // Quote by index
     if (args.index && !args.add && !args.remove && !args.list && !args.search && !args.purge) {
+      // Decrement the index to match the 0 indexing of the array
       let index = args.index - 1;
+
+      // If the index is in range of the quote array
       if (index >= 0 && index < allQuotes.length) {
         let msg = new MessageEmbed()
           .setColor(this.client.constants.infoEmbed)
@@ -175,20 +198,23 @@ class QuoteCommand extends Command {
 
         return message.channel.send(msg);
       } else {
+        // If there are no quotes in the server
         if (allQuotes.length == 0) {
           message.channel.send("There are no quotes for this server yet!");
         } else {
+          // If the specified index is out of range
           message.channel.send(`Please enter a number between 1 and ${allQuotes.length}`);
         }
       }
     }
 
-    // random quote
+    // Random quote
     if (!args.add && !args.remove && !args.list && !args.index && !args.search && !args.purge) {
-      let randIndex = Math.floor(Math.random() * allQuotes.length);
-      let randomQuote = allQuotes[randIndex];
-
+      // If the server has quotes
       if (allQuotes.length != 0) {
+        // Generate a random integer and pull the corresponding quote
+        let randIndex = Math.floor(Math.random() * allQuotes.length);
+        let randomQuote = allQuotes[randIndex];
         let msg = new MessageEmbed()
           .setColor(this.client.constants.infoEmbed)
           .setDescription(randomQuote)
@@ -197,6 +223,7 @@ class QuoteCommand extends Command {
 
         return message.channel.send(msg);
       } else {
+        // If there are no quotes in the server
         return message.channel.send("There are no quotes for this server yet!");
       }
     }
--- src/utilities/pagination.js
+++ src/utilities/pagination.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 const { MessageButton, MessageActionRow } = require("discord-buttons");
 
+// Used to format a page of quotes for the embed
 async function slice_quotes(quotes, page_index, og = null) {
   var page = quotes.slice(10 * (page_index - 1), 10 * page_index);
   if (!og) {
@@ -9,6 +10,7 @@ async function slice_quotes(quotes, page_index, og = null) {
   return page.map((q) => `\`${og.indexOf(q) + 1}.\` ${q}`).join("\n");
 }
 
+// Used to udate the page of quotes based
 async function update_page(button, embed, array, index, max, row, og = null) {
   embed.setDescription(await slice_quotes(array, index, og));
   embed.setFooter(`Page ${index}/${max}`);
@@ -16,6 +18,7 @@ async function update_page(button, embed, array, index, max, row, og = null) {
   await button.message.edit({ embed: embed, components: [row] });
 }
 
+// Used to delete quotes from the database
 async function purge_quotes(client, matches, og, message) {
   matches.forEach((q) => {
     og.splice(og.indexOf(q), 1);
@@ -29,19 +32,23 @@ async function purge_quotes(client, matches, og, message) {
 }
 
 module.exports = async function paginate(array, elements_per_page, starting_page, color, message, title, og = null, purge = false, client = null) {
+  // Ignore messages not containing integers and messages from everyone except the caller
   const message_filter = (m) => {
     var pattern = new RegExp("^\\d+$", "i");
     return pattern.test(m.content) && m.author == message.author.id;
   };
 
+  // Ignore button clicks from everyone except the caller
   const button_filter = (b) => {
     return b.clicker.user.id == message.author.id;
   };
-  var max_pages = Math.ceil(array.length / elements_per_page);
 
+  var max_pages = Math.ceil(array.length / elements_per_page);
   var page = await slice_quotes(array, starting_page, og);
 
   var embed = new MessageEmbed().setTitle(title).setDescription(page).setFooter(`Page ${starting_page}/${max_pages}`).setColor(color);
+
+  // Define buttons for interaction
   const next_button = new MessageButton().setEmoji("▶️").setStyle("grey").setID("next");
   const prev_button = new MessageButton().setEmoji("◀️").setStyle("grey").setID("prev");
   const jump_button = new MessageButton().setEmoji("🔼").setStyle("grey").setID("jump");
@@ -62,6 +69,7 @@ module.exports = async function paginate(array, elements_per_page, starting_page
       }
       switch (button.id) {
         case "next":
+          // Reset the page to 1 if it's on the last page
           page_index >= max_pages ? (page_index = 1) : page_index++;
 
           update_page(button, embed, array, page_index, max_pages, row, og);
@@ -69,6 +77,7 @@ module.exports = async function paginate(array, elements_per_page, starting_page
           button.defer();
           break;
         case "prev":
+          // Set the page to the last page if it's on the first page
           page_index <= 1 ? (page_index = max_pages) : page_index--;
 
           update_page(button, embed, array, page_index, max_pages, row, og);
@@ -76,17 +85,22 @@ module.exports = async function paginate(array, elements_per_page, starting_page
           button.defer();
           break;
         case "jump":
+          // Prompt user for a page to jump to
           button.reply.send(`What page would you like to jump to?`).then(() => {
             message.channel.awaitMessages(message_filter, { max: 1, time: 5000 }).then(async (collected) => {
+              // Cast the specified page to an int and round it
               var input = Math.round(Number(collected.first().content));
+              // If specified page is the current page, do nothing
               if (input == page_index) {
                 button.defer();
               } else {
+                // If the specified page is out of range
                 if (input < 1 || input > max_pages) {
                   message.channel.send(`Please enter a number between 1 and ${max_pages}.`).then((m) => m.delete({ timeout: 5000 }));
                   await button.reply.delete();
                   await collected.first().delete();
                 } else {
+                  // If the specified page is valid, update the page
                   page_index = input;
 
                   update_page(button, embed, array, page_index, max_pages, row, og);
@@ -99,9 +113,11 @@ module.exports = async function paginate(array, elements_per_page, starting_page
           });
           break;
         case "exit":
+          // Stop checking for interactions and delete the message
           await collector.stop();
           break;
         case "confirm":
+          // If user confirms the quote purge,then remove the matching quotes from the database
           purge_quotes(client, array, og, message);
           if (!button.replied) {
             await button.defer();
