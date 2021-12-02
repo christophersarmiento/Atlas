2fff35b4ad9b00fd21611f20b221c9978c9ea1f9
Christopher Sarmiento <chris@topher.ai>
2021-12-02 09:19:11 -0500

    Add error handling to TikTok listener.

--- src/listeners/TikTokListener.js
+++ src/listeners/TikTokListener.js
@@ -1,6 +1,7 @@
 const { Listener } = require("discord-akairo");
 const tiktok = require("tiktok-scraper");
 const { MessageEmbed, MessageAttachment } = require("discord.js");
+const { MessageButton, MessageActionRow } = require("discord-buttons");
 const axios = require("axios");
 
 class TikTokListener extends Listener {
@@ -11,17 +12,47 @@ class TikTokListener extends Listener {
     });
   }
 
+  embed_tt(url, message, loading_message, retry_button, button_filter, retry_count = 0) {
+    tiktok
+      .getVideoMeta(url)
+      .then((response) => {
+        axios.get(response.collector[0].videoUrl, { responseType: "arraybuffer", headers: response.headers }).then((r) => {
+          message.channel.send({ files: [{ attachment: r.data, name: response.collector[0].id + ".mp4" }] });
+        });
+        loading_message.delete();
+      })
+      .catch((err) => {
+        console.log(err);
+        if (retry_count >= 1) {
+          loading_message.edit("Sorry, could not load TikTok. (" + err + ")");
+        } else {
+          loading_message.edit("Uh oh! Something went wrong.", retry_button);
+        }
+
+        const collector = loading_message.createButtonCollector(button_filter, { time: 30000 });
+
+        collector.on("collect", async (button) => {
+          loading_message.edit("<a:loading:852240089765380096>");
+          retry_count++;
+          this.embed_tt(url, message, loading_message, retry_button, button_filter, retry_count);
+          button.defer();
+        });
+      });
+  }
+
   exec(message) {
     const regex = /https?:\/\/(vm.|www.)?tiktok.com\/\S+/g;
+    const retry_button = new MessageButton().setEmoji("🔄").setStyle("grey").setID("retry");
+    const button_filter = (b) => {
+      return b.clicker.user.id == message.author.id;
+    };
 
     var links = message.content.match(regex);
 
     if (links && !message.author.bot) {
-      links.forEach(async (link) => {
-        tiktok.getVideoMeta(link).then((response) => {
-          axios.get(response.collector[0].videoUrl, { responseType: "arraybuffer", headers: response.headers }).then((r) => {
-            message.channel.send({ files: [{ attachment: r.data, name: response.collector[0].id + ".mp4" }] });
-          });
+      message.channel.send("<a:loading:852240089765380096>").then((loading_message) => {
+        links.forEach(async (link) => {
+          this.embed_tt(link, message, loading_message, retry_button, button_filter);
         });
       });
     } else {
